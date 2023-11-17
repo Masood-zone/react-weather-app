83ce24bc13895eb327df293c741b09ba23f137d3
Masood <masoodacheampong@gmail.com>
2023-11-17 22:30:21 +0000

    Redesigning weather interface

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Home from "./pages/home";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <div className="">
+      <Header />
       <Home />
     </div>
   );
